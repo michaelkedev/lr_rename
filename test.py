b3621555a970091d2df08ff7cf87e79599971ea5
Michael Ke <k5855873@gmail.com>
2023-07-01 23:14:08 +0800

    perf : load image from xmp.thumbnail

--- test.py
+++ test.py
@@ -1,19 +1,39 @@
-import os
-from pyexiv2 import Image
+# import os
+# from pyexiv2 import Image
 
-image_format = ("jpg", "jpeg", "png")
-
-
-def get_rating(jpg_file):
-    if jpg_file.lower().endswith(image_format):
-        # print(jpg_file)
-        img = Image(jpg_file)
-        xmp_data = img.read_xmp()
-        img.close()
-
-        return xmp_data["Xmp.xmp.Rating"]
+# image_format = ("jpg", "jpeg", "png")
 
 
-files = os.listdir("./")
-for file in files:
-    print(get_rating(file))
+# def get_rating(jpg_file):
+#     if jpg_file.lower().endswith(image_format):
+#         # print(jpg_file)
+#         img = Image(jpg_file)
+#         xmp_data = img.read_xmp()
+#         img.close()
+
+#         return xmp_data["Xmp.xmp.Rating"]
+
+
+# files = os.listdir("./")
+# for file in files:
+#     print(get_rating(file))
+
+from exif import Image
+import piexif
+import struct
+
+# with open("./images/01.jpg", "rb") as f:
+#     my_image = Image(f)
+
+#     qp = QPixmap()
+#     qp.loadFromData(my_image.get_thumbnail())
+
+
+piexif_dict = piexif.load("./images/01.jpg")
+piexif_dict_byte = piexif.dump(piexif_dict)
+print(piexif_dict_byte)
+
+# b = piexif_dict["Exif"][18246]
+# count = len(b) / 2
+# rating = struct.unpack("H" * int(count), b)
+# print(rating)
